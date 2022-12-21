cfb99172192b439a07fa4b51533a986a9764b040
Hamza Zafar Bhatti <49752653+HamzaZafarBhatti@users.noreply.github.com>
2022-12-22 03:43:18 +0500

    Update sidebar.blade.php

--- resources/views/user/layout/sidebar.blade.php
+++ resources/views/user/layout/sidebar.blade.php
@@ -279,7 +279,11 @@
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link @if (Route::is('user.create_bank_details')) active @endif" href="{{ route('user.create_bank_details') }}">
+                    @if (!auth()->user()->bank)
+                        <a class="nav-link @if (Route::is('user.create_bank_details')) active @endif" href="{{ route('user.create_bank_details') }}">
+                    @else
+                        <a class="nav-link @if (Route::is('user.edit_bank_details', auth()->user()->bank->id)) active @endif" href="{{ route('user.edit_bank_details', auth()->user()->bank->id) }}">                        
+                    @endif
                         <i class="icon">
                             <svg width="22" viewBox="0 0 24 24" fill="none"
                                 xmlns="http://www.w3.org/2000/svg">
