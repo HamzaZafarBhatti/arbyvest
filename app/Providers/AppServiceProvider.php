d4c0fa79a97cbefacb2c612db520f32dbcde2e95
Hamza Zafar Bhatti <49752653+HamzaZafarBhatti@users.noreply.github.com>
2023-01-01 15:40:07 +0500

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -32,21 +32,21 @@ class AppServiceProvider extends ServiceProvider
     {
         //
         Schema::defaultStringLength(191);
-        // $blackmarketsales = BlackmarketLog::where('status', 0)->get();
-        // if (count($blackmarketsales) > 0) {
-        //     foreach ($blackmarketsales as $key => $value) {
-        //         Log::info($value);
-        //         $user = User::find($value->user_id);
-        //         $end_time = Carbon::parse($value->completed_at)->format("Y-m-d H:i:s");
-        //         Log::info(Carbon::now());
-        //         Log::info($end_time);
-        //         Log::info(json_encode(Carbon::now() > $end_time));
-        //         if (Carbon::now() > $end_time) {
-        //             Log::info('if 2');
-        //             $user->update(['ngn_wallet' => $user->ngn_wallet + $value->amount_exchanged]);
-        //             $value->update(['status' => 1]);
-        //         }
-        //     }
-        // }
+        $blackmarketsales = BlackmarketLog::where('status', 0)->get();
+        if (count($blackmarketsales) > 0) {
+            foreach ($blackmarketsales as $key => $value) {
+                Log::info($value);
+                $user = User::find($value->user_id);
+                $end_time = Carbon::parse($value->completed_at)->format("Y-m-d H:i:s");
+                Log::info(Carbon::now());
+                Log::info($end_time);
+                Log::info(json_encode(Carbon::now() > $end_time));
+                if (Carbon::now() > $end_time) {
+                    Log::info('if 2');
+                    $user->update(['ngn_wallet' => $user->ngn_wallet + $value->amount_exchanged]);
+                    $value->update(['status' => 1]);
+                }
+            }
+        }
     }
 }
